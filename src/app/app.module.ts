79b0a9e1508c9ec166953f8506fb1453609c4c30
DijanaKosovac <kosovacdijana@gmail.com>
2021-09-24 18:57:13 +0200

    Add core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { NavigationComponent } from './core/navigation/navigation.component';
+import { ShellComponent } from './core/shell/shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ShellComponent
   ],
   imports: [
     BrowserModule,
